600546cb66d25fe2969293c36d6d44d660a54454
Julien Dubocage <dubocage.julien68@gmail.com>
2022-11-24 14:12:18 +0100

    fix(front): corrige l'url de l'api en prod

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://met02-dubocage-julien.onrender.com/api/'
+  apiBaseUrl: 'https://met02-dubocage-julien.onrender.com/api'
 };
